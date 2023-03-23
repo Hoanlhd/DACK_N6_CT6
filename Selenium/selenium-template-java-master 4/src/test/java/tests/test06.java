9d433e7ac8f7915c88e82891454dd049bd3fba69
Hoanlhd <113487219+Hoanlhd@users.noreply.github.com>
2023-03-24 01:09:36 +0700

    Add files via upload

---
+++ Selenium/selenium-template-java-master 4/src/test/java/tests/test06.java
@@ -0,0 +1,41 @@
+package tests;
+
+import enums.Browser;
+import factory.BrowserProvider;
+import org.openqa.selenium.*;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+@Test
+public class test06 {
+    public static void main(String[] args) {
+        WebDriver driver = BrowserProvider.createDriver(Browser.CHROME);
+        try{
+            driver.get("https://thebestvape.vn/san-pham-moi");
+            WebElement select1 = driver.findElement(By.cssSelector("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > a:nth-child(1)"));
+            select1.click();
+            WebElement duahaulanh = driver.findElement(By.xpath("//a[@class='setCompare btn-views js-compare-product-add pd-compare-product-add']"));
+            duahaulanh.click();
+            System.out.println("Đã thêm Dưa hấu lạnh vào so sánh");
+            WebElement close = driver.findElement(By.cssSelector(".closeSidebar"));
+            close.click();
+            WebElement spmoi = driver.findElement(By.cssSelector("li[class='nav-item '] a[title='SẢN PHẨM MỚI']"));
+            spmoi.click();
+            WebElement select2 = driver.findElement(By.cssSelector("div[class='product-thumbnail'] a[title='Yogi Orchards - APPLE STRAWBERRY ICE ( Táo Dâu Tây Lạnh ) - Freebase']"));
+            select2.click();
+            WebElement daoxoailanh = driver.findElement(By.xpath("//a[@class='setCompare btn-views js-compare-product-add pd-compare-product-add']"));
+            daoxoailanh.click();
+            System.out.println("Đã thêm Đào xoài lạnh vào so sánh");
+            WebElement sosanh = driver.findElement(By.cssSelector("a[title='Bấm vào đây để tới trang so sánh'] span"));
+            sosanh.click();
+            WebElement home = driver.findElement(By.cssSelector("a[title='HOME']"));
+            home.click();
+            Thread.sleep(10000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        driver.quit();
+    }
+}
